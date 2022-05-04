01765d82a541fc2a69a11aa07545b979b32ce8b4
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-05-04 19:43:08 +0100

    Terraform Resource

---
+++ 04-01-Terraform-Resource/c2-ec2-instances.tf
@@ -0,0 +1,13 @@
+# Create EC2 Instance
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+
+resource "aws_instance" "my-ec2-vm" {
+  ami               = "ami-0c02fb55956c7d316"
+  instance_type     = "t2.micro"
+  #availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
+  tags = {
+    "Name" = "web"
+    "tag1" = "Update-test-1"
+  }
+}
