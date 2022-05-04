dbb9ecfe91740e7a62d39a3602bb362eb5f01772
ABDULRASAQ SALAUDEEN <salaudeen.abdulrasaq2021@gmail.com>
2022-05-04 19:45:28 +0100

    Revert "Terraform Meta-Arguments"
    This reverts commit ae21806bc38f098bbf051630e63d0356b0c1df53.

--- 04-03-Meta-Argument_Count & Count.Index/c2-ec2-instance.tf
+++
@@ -1,9 +0,0 @@
-# Create EC2 Instance
-resource "aws_instance" "web" {
-    ami = "ami-...." # Amazon Linux
-    instance_type = "t2.micro"
-    count = 5
-    tags = {
-        "Name" = "web-${count.index}"
-    }
-}
